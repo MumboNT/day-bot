c8a28fe6c7305a29cd5e84005affc5e4309b8b61
Manning Meindel (UWL) <meindel.manning@uwlax.edu>
2019-01-03 12:47:55 -0600

    t

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('ready', () => {
    //Displays time until next update is posted.
    if (msg.content.startsWith("!NextUpdate") && msg.channel.id === id) {
      date = new Date();
-     msg.channel.send((23-((date.getHours()))+6) + " Hours, " + (59 - (date.getMinutes())) + " minutes, and " + (60 - (date.getSeconds())) + " seconds until 12AM");
+     msg.channel.send((23-((date.getHours())+6)) + " Hours, " + (59 - (date.getMinutes())) + " minutes, and " + (60 - (date.getSeconds())) + " seconds until 12AM");
      msg.channel.send(minsToMidnight());
    }
    //Help
@@ -103,7 +103,7 @@ client.on('ready', () => {
      getForecast();
    }, 24*3600000); // time between each interval in milliseconds
 
- }, ((23 - (new Date().getHours()))*60*60+(59 - (new Date().getMinutes()))*60+(60 - (new Date().getSeconds())))*1000);
+ }, ((23 - (new Date().getHours()+6))*60*60+(59 - (new Date().getMinutes()))*60+(60 - (new Date().getSeconds())))*1000);
 
 });
 
@@ -695,7 +695,7 @@ function getBAD(){
 function minsToMidnight(){
   var now = new Date();
   var then = new Date(now);
-  then.setHours(11, 0, 0, 0);
+  then.setHours(23, 0, 0, 0);
   return Math.abs((then - now) / 6e4);
   //return Math.round((msd-(now-now.getTimezoneOffset() * 6e4) % msd) / 6e4);
 }
